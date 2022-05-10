6fabfac603fa5b109d53e49a09125ea88b965a47
Reece Orbuck <reeceorbuck@me.com>
2022-05-10 20:35:12 +0800

    use direct link to typescript types

--- src/tsAdapter.ts
+++ src/tsAdapter.ts
@@ -1,3 +1,3 @@
-// @deno-types="../nodeCompatibility/node_modules/typescript/lib/typescript.d.ts"
+// @deno-types="https://raw.githubusercontent.com/microsoft/TypeScript/main/lib/typescript.d.ts";
 import ts from '../nodeCompatibility/exports.ts';
 export default ts;
