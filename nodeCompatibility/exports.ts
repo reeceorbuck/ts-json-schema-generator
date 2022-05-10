ad45a1ef37c481366ac43c07349b52614d4a213a
Reece Orbuck <reeceorbuck@me.com>
2022-05-10 19:53:57 +0800

    global typescript

--- nodeCompatibility/exports.ts
+++ nodeCompatibility/exports.ts
@@ -1,21 +1,9 @@
 import { createRequire } from 'https://deno.land/std@0.138.0/node/module.ts';
 import url from 'https://deno.land/std@0.138.0/node/url.ts';
-// import * as path from 'https://deno.land/std@0.138.0/path/mod.ts';
-// import * as url from 'https://deno.land/std@0.138.0/node/url.ts';
 
-// console.log('import.meta.url: ', url.pathToFileURL(import.meta.url));
-// internal import.meta.url:  file:///Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts
-// external import.meta.url:  https://raw.githubusercontent.com/reeceorbuck/ts-json-schema-generator/next/nodeCompatibility/exports.ts
-const hardCode = '/Users/reeceorbuck/Library/Caches/deno/gen/file/Users/reeceorbuck/Projects/ts-json-schema-generator/';
+const globalTypescript = '/opt/homebrew/lib/';
 
-const baseHardCode = '/Users/reeceorbuck/Projects/typescript-json-schema/';
-// const p = path.toFileUrl('/home/foo');
+console.log('globalTypescript url: ', url.pathToFileURL(globalTypescript));
 
-// path.toFileUrl('/var/tmp/a.log');
-
-// console.log('test: ', new URL('static-files', import.meta.url).pathname);
-
-console.log('hardCode url href: ', url.pathToFileURL(baseHardCode).href);
-
-const require = createRequire(url.pathToFileURL(baseHardCode).href);
+const require = createRequire(url.pathToFileURL(globalTypescript));
 export default require('typescript');
