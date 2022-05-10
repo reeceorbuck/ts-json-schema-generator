17c61ac44113561c923ce3b8eedc9654db2c2f0c
Reece Orbuck <reeceorbuck@me.com>
2022-05-10 20:01:48 +0800

    logging

--- factory/program.ts
+++ factory/program.ts
@@ -1,92 +1,93 @@
-import * as path from "https://deno.land/std@0.138.0/path/mod.ts";
-import { walkSync } from "https://deno.land/std@0.136.0/fs/mod.ts";
+import * as path from 'https://deno.land/std@0.138.0/path/mod.ts';
+import { walkSync } from 'https://deno.land/std@0.136.0/fs/mod.ts';
 
-import ts from "../src/tsAdapter.ts";
+import ts from '../src/tsAdapter.ts';
 
-import { Config } from "../src/Config.ts";
-import { DiagnosticError } from "../src/Error/DiagnosticError.ts";
-import { LogicError } from "../src/Error/LogicError.ts";
-import { NoRootNamesError } from "../src/Error/NoRootNamesError.ts";
-import { NoTSConfigError } from "../src/Error/NoTSConfigError.ts";
+import { Config } from '../src/Config.ts';
+import { DiagnosticError } from '../src/Error/DiagnosticError.ts';
+import { LogicError } from '../src/Error/LogicError.ts';
+import { NoRootNamesError } from '../src/Error/NoRootNamesError.ts';
+import { NoTSConfigError } from '../src/Error/NoTSConfigError.ts';
 
 function loadTsConfigFile(configFile: string) {
-  const raw = ts.sys.readFile(configFile);
-  if (raw) {
-    const config = ts.parseConfigFileTextToJson(configFile, raw);
+	const raw = ts.sys.readFile(configFile);
+	if (raw) {
+		const config = ts.parseConfigFileTextToJson(configFile, raw);
 
-    if (config.error) {
-      throw new DiagnosticError([config.error]);
-    } else if (!config.config) {
-      throw new LogicError(`Invalid parsed config file "${configFile}"`);
-    }
+		if (config.error) {
+			throw new DiagnosticError([config.error]);
+		} else if (!config.config) {
+			throw new LogicError(`Invalid parsed config file "${configFile}"`);
+		}
 
-    const parseResult = ts.parseJsonConfigFileContent(
-      config.config,
-      ts.sys,
-      path.resolve(path.dirname(configFile)),
-      {},
-      configFile,
-    );
-    parseResult.options.noEmit = true;
-    delete parseResult.options.out;
-    delete parseResult.options.outDir;
-    delete parseResult.options.outFile;
-    delete parseResult.options.declaration;
-    delete parseResult.options.declarationDir;
-    delete parseResult.options.declarationMap;
+		const parseResult = ts.parseJsonConfigFileContent(
+			config.config,
+			ts.sys,
+			path.resolve(path.dirname(configFile)),
+			{},
+			configFile,
+		);
+		parseResult.options.noEmit = true;
+		delete parseResult.options.out;
+		delete parseResult.options.outDir;
+		delete parseResult.options.outFile;
+		delete parseResult.options.declaration;
+		delete parseResult.options.declarationDir;
+		delete parseResult.options.declarationMap;
 
-    return parseResult;
-  } else {
-    throw new NoTSConfigError();
-  }
+		return parseResult;
+	} else {
+		throw new NoTSConfigError();
+	}
 }
 
 function getTsConfig(config: Config) {
-  if (config.tsconfig) {
-    return loadTsConfigFile(config.tsconfig);
-  }
+	if (config.tsconfig) {
+		return loadTsConfigFile(config.tsconfig);
+	}
 
-  return {
-    fileNames: [],
-    options: {
-      noEmit: true,
-      emitDecoratorMetadata: true,
-      experimentalDecorators: true,
-      target: ts.ScriptTarget.ES5,
-      module: ts.ModuleKind.CommonJS,
-      strictNullChecks: false,
-    },
-  };
+	return {
+		fileNames: [],
+		options: {
+			noEmit: true,
+			emitDecoratorMetadata: true,
+			experimentalDecorators: true,
+			target: ts.ScriptTarget.ES5,
+			module: ts.ModuleKind.CommonJS,
+			strictNullChecks: false,
+		},
+	};
 }
 
 export function createProgram(config: Config): ts.Program {
-  const files = [...walkSync(path.normalize(path.resolve()), {
-    match: [path.globToRegExp(path.normalize(path.resolve(config.path!)))],
-    includeDirs: false,
-  })].map((dirEntry) => path.normalize(dirEntry.path));
+	console.log('path.resolve(): ', path.resolve());
+	const files = [...walkSync(path.normalize(path.resolve()), {
+		match: [path.globToRegExp(path.normalize(path.resolve(config.path!)))],
+		includeDirs: false,
+	})].map((dirEntry) => path.normalize(dirEntry.path));
 
-  const rootNamesFromPath = config.path ? files : [];
-  const tsconfig = getTsConfig(config);
+	console.log('files: ', files);
 
-  const rootNames = rootNamesFromPath.length
-    ? rootNamesFromPath
-    : tsconfig.fileNames;
+	const rootNamesFromPath = config.path ? files : [];
+	const tsconfig = getTsConfig(config);
 
-  if (!rootNames.length) {
-    throw new NoRootNamesError();
-  }
+	const rootNames = rootNamesFromPath.length ? rootNamesFromPath : tsconfig.fileNames;
 
-  const program: ts.Program = ts.createProgram(
-    rootNames as string[],
-    tsconfig.options,
-  );
+	if (!rootNames.length) {
+		throw new NoRootNamesError();
+	}
 
-  if (!config.skipTypeCheck) {
-    const diagnostics = ts.getPreEmitDiagnostics(program);
-    if (diagnostics.length) {
-      throw new DiagnosticError(diagnostics);
-    }
-  }
+	const program: ts.Program = ts.createProgram(
+		rootNames as string[],
+		tsconfig.options,
+	);
 
-  return program;
+	if (!config.skipTypeCheck) {
+		const diagnostics = ts.getPreEmitDiagnostics(program);
+		if (diagnostics.length) {
+			throw new DiagnosticError(diagnostics);
+		}
+	}
+
+	return program;
 }
--- nodeCompatibility/exports.ts
+++ nodeCompatibility/exports.ts
@@ -3,7 +3,5 @@ import url from 'https://deno.land/std@0.138.0/node/url.ts';
 
 const globalTypescript = '/opt/homebrew/lib/';
 
-console.log('globalTypescript url: ', url.pathToFileURL(globalTypescript));
-
 const require = createRequire(url.pathToFileURL(globalTypescript));
 export default require('typescript');
