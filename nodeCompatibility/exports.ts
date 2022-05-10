74cf6d3ff09313b00a4fe5fc83bf704fee4a5789
Reece Orbuck <reeceorbuck@me.com>
2022-05-10 12:45:08 +0800

    testing

--- nodeCompatibility/exports.ts
+++ nodeCompatibility/exports.ts
@@ -1,12 +1,18 @@
 import { createRequire } from 'https://deno.land/std@0.138.0/node/module.ts';
+import { fromMeta } from 'https://x.nest.land/dirname_deno@0.3.0/mod.ts';
+// or from "https://deno.land/x/dirname_deno@v0.3.0/mod.ts"
+
+const { __dirname, __filename } = fromMeta(import.meta);
+
+console.log(__dirname); // /home/you/projects/project/src
+console.log(__filename); // /home/you/projects/project/src/main.ts
 
 console.log('import.meta.url: ', import.meta.url);
-// metaURL:  file:///Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts
+// internal import.meta.url:  file:///Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts
+// external import.meta.url:  https://raw.githubusercontent.com/reeceorbuck/ts-json-schema-generator/next/nodeCompatibility/exports.ts
+// "/Users/reeceorbuck/Library/Caches/deno/gen/file/Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts.js"
 
-console.log(
-	'Deno.mainModule: ',
-	Deno.mainModule,
-);
+console.log('test: ', new URL('static-files', import.meta.url).pathname);
 
 const require = createRequire(import.meta.url);
 export default require('typescript');
