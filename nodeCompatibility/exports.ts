d45059f0fac6b92a9fca5eb179f82d9cfbcacafe
Reece Orbuck <reeceorbuck@me.com>
2022-05-10 12:59:14 +0800

    p

--- nodeCompatibility/exports.ts
+++ nodeCompatibility/exports.ts
@@ -1,13 +1,16 @@
 import { createRequire } from 'https://deno.land/std@0.138.0/node/module.ts';
+import * as path from 'https://deno.land/std@0.138.0/path/mod.ts';
 
 console.log('import.meta.url: ', import.meta.url);
 // internal import.meta.url:  file:///Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts
 // external import.meta.url:  https://raw.githubusercontent.com/reeceorbuck/ts-json-schema-generator/next/nodeCompatibility/exports.ts
 // "/Users/reeceorbuck/Library/Caches/deno/gen/file/Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts.js"
 
+const p = path.toFileUrl('/home/foo');
+
 console.log(
-	'Deno.mainModule: ',
-	Deno.mainModule,
+	'p: ',
+	p,
 );
 console.log('test: ', new URL('static-files', import.meta.url).pathname);
 
