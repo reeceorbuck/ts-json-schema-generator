38449a80ce9f8e373520bbc3616f432252f4ad16
Reece Orbuck <reeceorbuck@me.com>
2022-05-10 13:08:08 +0800

    path

--- nodeCompatibility/exports.ts
+++ nodeCompatibility/exports.ts
@@ -1,7 +1,8 @@
 import { createRequire } from 'https://deno.land/std@0.138.0/node/module.ts';
 import * as path from 'https://deno.land/std@0.138.0/path/mod.ts';
+import * as url from 'https://deno.land/std@0.138.0/node/url.ts';
 
-console.log('import.meta.url: ', import.meta.url);
+console.log('import.meta.url: ', url.pathToFileURL(import.meta.url));
 // internal import.meta.url:  file:///Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts
 // external import.meta.url:  https://raw.githubusercontent.com/reeceorbuck/ts-json-schema-generator/next/nodeCompatibility/exports.ts
 // "/Users/reeceorbuck/Library/Caches/deno/gen/file/Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts.js"
@@ -10,8 +11,11 @@ const p = path.toFileUrl('/home/foo');
 
 console.log(
 	'p: ',
-	p,
+	path.dirname(),
 );
+
+path.toFileUrl('/var/tmp/a.log');
+
 console.log('test: ', new URL('static-files', import.meta.url).pathname);
 
 console.log('file://$cwd: ', Deno.cwd());
