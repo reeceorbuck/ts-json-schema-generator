ca54c56c495b8296a41a39cf73c9ac690599207a
Reece Orbuck <reeceorbuck@me.com>
2022-05-10 16:54:08 +0800

    hardcode root

--- nodeCompatibility/exports.ts
+++ nodeCompatibility/exports.ts
@@ -6,8 +6,7 @@ import url from 'https://deno.land/std@0.138.0/node/url.ts';
 // console.log('import.meta.url: ', url.pathToFileURL(import.meta.url));
 // internal import.meta.url:  file:///Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts
 // external import.meta.url:  https://raw.githubusercontent.com/reeceorbuck/ts-json-schema-generator/next/nodeCompatibility/exports.ts
-const hardCode =
-	'/Users/reeceorbuck/Library/Caches/deno/gen/file/Users/reeceorbuck/Projects/ts-json-schema-generator/nodeCompatibility/exports.ts.js';
+const hardCode = '/Users/reeceorbuck/Library/Caches/deno/gen/file/Users/reeceorbuck/Projects/ts-json-schema-generator/';
 
 // const p = path.toFileUrl('/home/foo');
 
