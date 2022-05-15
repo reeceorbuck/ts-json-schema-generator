7c12f862ae1ae1955728267f7c8ff1720eb3036a
Reece Orbuck <reeceorbuck@me.com>
2022-05-15 11:20:07 +0800

    better handling of failures

--- nodeCompatibility/exports.ts
+++ nodeCompatibility/exports.ts
@@ -2,30 +2,47 @@ import { createRequire } from 'https://deno.land/std@0.138.0/node/module.ts';
 import url from 'https://deno.land/std@0.138.0/node/url.ts';
 import tempDir from 'https://deno.land/x/temp_dir@v1.0.0/mod.ts';
 import { writeAll } from 'https://deno.land/std@0.138.0/streams/mod.ts';
-
-const _globalTypescript = '/opt/homebrew/lib/';
+import { copy } from 'https://deno.land/std@0.139.0/fs/copy.ts';
 
 const tempDirPath = tempDir;
+let module;
+
+// try {
+// 	await Deno.stat(url.pathToFileURL(`${tempDirPath}/node_modules/typescript`));
+// 	console.log('Temp typescript module already exists, no need to fetch it again.');
+// } catch {
 
 try {
-	await Deno.stat(url.pathToFileURL(`${tempDirPath}/node_modules/typescript`));
-	console.log('Temp typescript module already exists, no need to fetch it again.');
+	await Deno.remove(`${tempDirPath}/node_modules/typescriptBackup`, { recursive: true });
+} catch {
+	// Only errors if the folder doesnt exist, which is fine.
+	// console.log('Failed to remove existing: ', err);
+}
+
+const require = createRequire(url.pathToFileURL(`${tempDirPath}/`));
+
+try {
+	module = require('typescript');
 } catch {
 	console.log('Fetching Typescript dependency for caching...');
+
+	try {
+		await Deno.remove(`${tempDirPath}/node_modules/typescript`, { recursive: true });
+	} catch {
+		// console.log('Failed to remove existing: ', err);
+	}
+
 	const response = await fetch('https://github.com/reeceorbuck/typscriptModule/zipball/main');
 	const blob = await response.blob();
 	const buf = await blob.arrayBuffer();
 	const data = new Uint8Array(buf);
 	// We then create a new file and write into it
-	const file = await Deno.create(url.pathToFileURL(`${tempDirPath}/typescript.zip`));
+	const file = await Deno.create(`${tempDirPath}/typescript.zip`);
 	await writeAll(file, data);
 	Deno.close(file.rid);
 
 	const zipSourcePath = url.pathToFileURL(`${tempDirPath}/typescript.zip`);
 	const destinationPath = url.pathToFileURL(`${tempDirPath}/node_modules`);
-
-	console.log('zipSourcePath: ', zipSourcePath);
-	console.log('destinationPath: ', destinationPath);
 
 	const p = Deno.run({
 		cmd: Deno.build.os === 'windows'
@@ -42,22 +59,29 @@ try {
 		stderr: 'piped',
 	});
 
-	const [status] = await Promise.all([
+	await Promise.all([
 		p.status(),
 		p.output(),
 		p.stderrOutput(),
 	]);
 	p.close();
 
-	console.log('status of unzip operation: ', status);
-
-	await Deno.remove(url.pathToFileURL(`${tempDirPath}/typescript.zip`));
+	await Deno.remove(`${tempDirPath}/typescript.zip`);
 
 	await Deno.rename(
-		url.pathToFileURL(`${tempDirPath}/node_modules/reeceorbuck-typscriptModule-8f10914`),
-		url.pathToFileURL(`${tempDirPath}/node_modules/typescript`),
+		`${tempDirPath}/node_modules/reeceorbuck-typscriptModule-8f10914`,
+		`${tempDirPath}/node_modules/typescript`,
 	);
+	// There is an issue where the createRequire must cache the first time we try to get typescript
+	// So we copy to a different name so can get a fresh import
+	// Next time this runs it cleans up the backup folder for us and uses the original which will now work
+	await copy(`${tempDirPath}/node_modules/typescript`, `${tempDirPath}/node_modules/typescriptBackup`);
+
+	try {
+		module = require('typescriptBackup');
+	} catch (err) {
+		console.error('Still failed after rebuild of typescript module, err: ', err);
+	}
 }
 
-const require = createRequire(url.pathToFileURL(`${tempDirPath}/`));
-export default require('typescript');
+export default module;
--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 import * as tsj from './index.ts';
+import tempDir from 'https://deno.land/x/temp_dir@v1.0.0/mod.ts';
+
 const config = {
 	path: 'input.d.ts',
 	type: '*', // Or <type-name> if you want to generate schema for that one type only
 };
 
-const output_path = 'output.json';
+console.log('Deno temp dir: ', tempDir);
+
+const output_path = 'outputB.json';
 
 const schema = tsj.createGenerator(config).createSchema(config.type);
 const schemaString = JSON.stringify(schema, null, 2);
